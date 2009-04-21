64eac744bfcd036d7fa3f858a09386deec2fd9bd
tlrobinson <tom@280north.com>
2009-04-21 00:18:24 -0700

    * Fixed bug in autodetection of jackconfig module. * Moved sameple jackconfig.js to root of Jack project. * Added jackup shebang to samepl jackconfig.js and explicitly import used middleware.

--- example/jackconfig.js
+++
@@ -1,9 +0,0 @@
-var Jack = require("jack");
-
-exports.app = function(env) {
-    return [200, {"Content-Type":"text/plain"}, "Hello world!"];
-}
-
-exports.development = function(app) {
-    return Jack.CommonLogger(Jack.ShowExceptions(Jack.Lint(Jack.ContentLength(app))));
-}
