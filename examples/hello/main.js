f3920eb0d21184cc4e61b3a31ed0d29a3edec276
kriskowal <kris.kowal@cixar.com>
2009-05-27 14:33:12 -0700

    Fixed a bug in the Hello, World! example which caused reloading jackup.

--- examples/hello/main.js
+++ examples/hello/main.js
@@ -5,6 +5,6 @@ exports.app = new ContentLength(function (env) {
     return [200, {"content-type": "text/plain"}, ["Hello, World!"]];
 });
 
-if (require.id = require.main)
+if (require.id == require.main)
     require('jackup').main([require.id].concat(system.args));
 
