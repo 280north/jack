d7b5ca58f431de3a1e6a3a8eb791f4092b1fab3a
tlrobinson <tom@280north.com>
2009-06-02 01:33:54 -0700

    Updated example jackconfig.js to wrap body in an array. Added comments.

--- jackconfig.js
+++ jackconfig.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env jackup
 
+// the "app" export is the default property used by jackup:
 exports.app = function(env) {
-    return [200, {"Content-Type":"text/plain"}, "Hello worasdfld!"];
+    return [200, {"Content-Type":"text/plain"},
+        ["jackconfig.js is the default file jackup looks for!"]];
 }
 
+// specify custom sets of middleware and initialization routines
+// by defining a function with the same name as the environment:
 exports.development = function(app) {
     return require("jack/commonlogger").CommonLogger(
         require("jack/showexceptions").ShowExceptions(
