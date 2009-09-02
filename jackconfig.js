9cb8d6abaf26da81e12297f7d548cac2f2dec3b3
tlrobinson <tom@280north.com>
2009-09-02 00:03:56 -0700

    JSGI spec change: response is Object {status:,headers:,body:} instead of Array [,,]

--- jackconfig.js
+++ jackconfig.js
@@ -2,8 +2,11 @@
 
 // the "app" export is the default property used by jackup:
 exports.app = function(env) {
-    return [200, {"Content-Type":"text/plain"},
-        ["jackconfig.js is the default file jackup looks for!"]];
+    return {
+        status : 200,
+        headers : {"Content-Type":"text/plain"},
+        body : ["jackconfig.js is the default file jackup looks for!"]
+    };
 }
 
 // specify custom sets of middleware and initialization routines
