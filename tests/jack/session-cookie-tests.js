fbe6bf2a28d1eef53ccde8e1b64bedcad52a285c
Nathan <nathan@Planck.(none)>
2009-09-14 08:54:38 -0400

    refactored session cookie to make it return a function that takes env like the other jack middleware

--- tests/jack/session-cookie-tests.js
+++ tests/jack/session-cookie-tests.js
@@ -1,20 +1,33 @@
 var assert = require("test/assert"),
+    Jack = require("jack"),
     Request = require("jack/request").Request,
     Response = require("jack/response").Response,
     MockRequest = require("jack/mock").MockRequest,
     Cookie = require("jack/session/cookie").Cookie;
 
+var helloApp = function(block) {
+    return Jack.URLMap({
+        "/": function(env) {
+            block(env);
+            return {
+                status: 200,
+                headers: { 'Content-Type': 'text/html'},
+                body: ["hello"]
+            };
+        }
+    });
+};
+
 exports.testCreatesNewCookie = function(){
     var env = MockRequest.envFor(null, "", {});
 
-    var cookie = new Cookie(function(env) {
+    var setMyKey = function(env){
         env["jsgi.session"]["mykey"] = "myval";
-        return new Response(env).finish();
-    }, { secret: "secret" });
+    };
 
-    var response = new MockRequest(function(env) {
-        return cookie.run(env);
-    }).GET("/");
+    var app = helloApp(setMyKey);
+
+    var response = new MockRequest(new Cookie(app, { secret: "secret" })).GET("/");
 
     assert.isTrue(response.headers["Set-Cookie"] != undefined, "Should have defined 'Set-Cookie'");
     assert.isTrue(response.headers["Set-Cookie"].match(/jsgi.session=/g) != null, "Should have created a new cookie");
@@ -23,12 +36,13 @@ exports.testCreatesNewCookie = function(){
 exports.testRetrieveSessionValue = function(){
     var retrievedVal = null;
 
-    new MockRequest(function(env){
-        return new Cookie(function(env) {
-            retrievedVal = env["jsgi.session"]["mykey"];
-            return new Response(env).finish();
-        },{ secret: "secret" }).run(env);
-    }).GET("/", { "HTTP_COOKIE": "jsgi.session=%7B%22mykey%22%3A%22myval%22%7D--2m6GuCKsHcPfqaI2Yezhy7kdo%2Fg%3D" });
+    var retrieveMyKey = function(env) {
+        retrievedVal = env["jsgi.session"]["mykey"];
+    };
+
+    var app = helloApp(retrieveMyKey);
+
+    new MockRequest(new Cookie(app, {secret: "secret" })).GET("/", { "HTTP_COOKIE": "jsgi.session=%7B%22mykey%22%3A%22myval%22%7D--2m6GuCKsHcPfqaI2Yezhy7kdo%2Fg%3D" });
 
     assert.isEqual("myval", retrievedVal);
 }
