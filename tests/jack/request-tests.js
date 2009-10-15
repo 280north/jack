116508806ce69c8a490f7d41b706f96ef5cee260
Tom Robinson <tom@280north.com>
2009-10-14 18:17:12 -0700

    Change body() to read, cache, and return ByteString. Implement hasFormData() and friends.

--- tests/jack/request-tests.js
+++ tests/jack/request-tests.js
@@ -16,3 +16,33 @@ exports.testParseCookiesRFC2109 = function() {
     
     assert.isSame({"foo" : "bar"}, req.cookies());
 }
+
+exports.testMediaType = function() {
+    var req = new Request(MockRequest.envFor(null, "", { "CONTENT_TYPE" : "text/html" }));
+    assert.isEqual(req.mediaType(), "text/html");
+    assert.isEqual(req.contentType(), "text/html");
+    
+    var req = new Request(MockRequest.envFor(null, "", { "CONTENT_TYPE" : "text/html; charset=utf-8" }));
+    assert.isEqual(req.mediaType(), "text/html");
+    assert.isEqual(req.contentType(), "text/html; charset=utf-8");
+    assert.isEqual(req.mediaTypeParams()["charset"], "utf-8");
+    assert.isEqual(req.contentCharset(), "utf-8");
+    
+    var req = new Request(MockRequest.envFor(null, "", {}));
+    assert.isEqual(req.mediaType(), null);
+    assert.isEqual(req.contentType(), null);
+}
+
+exports.testHasFormData = function() {
+    var req = new Request(MockRequest.envFor(null, "", { "CONTENT_TYPE" : "application/x-www-form-urlencoded" }));
+    assert.isEqual(req.hasFormData(), true);
+    
+    var req = new Request(MockRequest.envFor(null, "", { "CONTENT_TYPE" : "multipart/form-data" }));
+    assert.isEqual(req.hasFormData(), true);
+    
+    var req = new Request(MockRequest.envFor(null, "", {}));
+    assert.isEqual(req.hasFormData(), true);
+    
+    var req = new Request(MockRequest.envFor(null, "", { "CONTENT_TYPE" : "text/html" }));
+    assert.isEqual(req.hasFormData(), false);
+}
