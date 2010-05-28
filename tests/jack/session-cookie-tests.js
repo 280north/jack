5f4180a772c965f65069ad6ab281fd9697213287
Tom Robinson <tom@280north.com>
2010-05-27 19:35:01 -0700

    Fix Session test and do style cleanup / contrib annotations for session, session/cookie, and session test

--- tests/jack/session-cookie-tests.js
+++ tests/jack/session-cookie-tests.js
@@ -1,50 +1,48 @@
-var assert = require("test/assert"),
-    Jack = require("jack"),
-    Request = require("jack/request").Request,
-    Response = require("jack/response").Response,
-    MockRequest = require("jack/mock").MockRequest,
-    Cookie = require("jack/session/cookie").Cookie;
 
-var helloApp = function(block) {
-    return Jack.URLMap({
-        "/": function(env) {
-            block(env);
-            return {
-                status: 200,
-                headers: { 'Content-Type': 'text/html'},
-                body: ["hello"]
-            };
-        }
-    });
+// -- nrstott Nathan Stott
+// -- paulbaumgart Paul Baumgart
+// -- tlrobinson Tom Robinson Copyright (C) 2009-2010 MIT License
+
+var ASSERT = require("test/assert");
+
+var Request = require("jack/request").Request;
+var Response = require("jack/response").Response;
+var Cookie = require("jack/session/cookie").Cookie;
+var MockRequest = require("jack/mock").MockRequest;
+
+function helloApp(block) {
+    return function(env) {
+        block(env);
+        return {
+            status: 200,
+            headers: { 'Content-Type': 'text/html'},
+            body: ["hello"]
+        };
+    }
 };
 
-exports.testCreatesNewCookie = function(){
-    var env = MockRequest.envFor(null, "", {});
-
-    var setMyKey = function(env){
+exports.testCreatesNewCookie = function() {
+    var app = helloApp(function(env) {
+        env["jsgi.session.load"]();
         env["jsgi.session"]["mykey"] = "myval";
-    };
+    });
 
-    var app = helloApp(setMyKey);
+    var response = new MockRequest(Cookie(app, { secret: "secret" })).GET("/");
 
-    var response = new MockRequest(new Cookie(app, { secret: "secret" })).GET("/");
-
-    assert.isTrue(response.headers["Set-Cookie"] != undefined, "Should have defined 'Set-Cookie'");
-    assert.isTrue(response.headers["Set-Cookie"].match(/jsgi.session=/g) != null, "Should have created a new cookie");
+    ASSERT.isTrue(response.headers["Set-Cookie"] != undefined, "Should have defined 'Set-Cookie'");
+    ASSERT.isTrue(response.headers["Set-Cookie"].match(/jsgi.session=/g) != null, "Should have created a new cookie");
 }
 
-exports.testRetrieveSessionValue = function(){
+exports.testRetrieveSessionValue = function() {
     var retrievedVal = null;
-
-    var retrieveMyKey = function(env) {
+    var app = helloApp(function(env) {
+        env["jsgi.session.load"]();
         retrievedVal = env["jsgi.session"]["mykey"];
-    };
+    });
 
-    var app = helloApp(retrieveMyKey);
+    new MockRequest(Cookie(app, {secret: "secret" })).GET("/", { "HTTP_COOKIE": "jsgi.session=%7B%22mykey%22%3A%22myval%22%7D--2m6GuCKsHcPfqaI2Yezhy7kdo%2Fg%3D" });
 
-    new MockRequest(new Cookie(app, {secret: "secret" })).GET("/", { "HTTP_COOKIE": "jsgi.session=%7B%22mykey%22%3A%22myval%22%7D--2m6GuCKsHcPfqaI2Yezhy7kdo%2Fg%3D" });
-
-    assert.isEqual("myval", retrievedVal);
+    ASSERT.isEqual("myval", retrievedVal);
 }
 
 if (require.main === module)
